d551eaddb751bbcead921312410f224476b14e4f
Andresv309 <andresv309@gmail.com>
2023-12-14 05:25:51 -0500

    Implement interfaces and add bussiness logic

---
+++ Server/ArtLite.Api/Services/Artwork/ArtworkService.cs
@@ -0,0 +1,99 @@
+﻿using ArtLite.Api.Entities;
+using ArtLite.Api.Persistence;
+using ArtLite.Api.ServiceErrors;
+using ErrorOr;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtLite.Api.Services;
+
+public class ArtworkService : IArtworkService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public ArtworkService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<ErrorOr<Artwork>> AddArtwork(Artwork newArtwork)
+    {
+        var tagsList = newArtwork.Tags
+            .Select(tag => tag.TagName);
+
+        if (tagsList is not null)
+        {
+            var selectedTags = _dbContext.Tags
+                .Where(t => tagsList.Contains(t.TagName)).ToList();
+
+            newArtwork.Tags = selectedTags;
+        }
+
+        await _dbContext.AddAsync(newArtwork);
+        await _dbContext.SaveChangesAsync();
+
+        return newArtwork;
+    }
+
+    public async Task<ErrorOr<Deleted>> DeleteArtwork(Guid id)
+    {
+        var artwork = await _dbContext.Artworks
+            .FindAsync(id);
+
+        if (artwork is null)
+        {
+            return ArtworkError.NotFound;
+        }
+        
+        _dbContext.Remove(artwork);
+        await _dbContext.SaveChangesAsync();
+
+        return Result.Deleted;
+    }
+
+    public async Task<ErrorOr<Artwork>> GetArtworkById(Guid id)
+    {
+        var artwork = await _dbContext.Artworks
+            .Include(a => a.Creator)
+            .Include(a => a.Tags)
+            .Include(a => a.Images)
+            .SingleOrDefaultAsync(a => a.IdArtwork == id);
+
+
+        if (artwork is null)
+        {
+            return ArtworkError.NotFound;
+        }
+
+        return artwork;
+    }
+
+    public async Task<ErrorOr<Artwork>> UpdateArtwork(Guid id, Artwork updatedArtwork)
+    {
+        var artwork = await _dbContext.Artworks
+            .Include(a => a.Tags)
+            .SingleOrDefaultAsync(a => a.IdArtwork == id);
+
+
+        if (artwork is null)
+        {
+            return ArtworkError.NotFound;
+        }
+
+        var tagsList = updatedArtwork.Tags?.Select(tag => tag.TagName);
+
+        if (tagsList is not null)
+        {
+            var selectedTags = _dbContext.Tags
+                .Where(t => tagsList.Contains(t.TagName)).ToList();
+
+            artwork.Tags = selectedTags;
+        }
+
+        artwork.Title = updatedArtwork.Title;
+        artwork.Description = updatedArtwork.Description;
+
+        await _dbContext.SaveChangesAsync();
+
+        return artwork;
+    }
+}
---
+++ Server/ArtLite.Api/Services/Creator/CreatorService.cs
@@ -0,0 +1,47 @@
+﻿using ArtLite.Api.Entities;
+using ArtLite.Api.Persistence;
+using ArtLite.Api.ServiceErrors;
+using ErrorOr;
+using Microsoft.EntityFrameworkCore;
+
+namespace ArtLite.Api.Services;
+
+public class CreatorService : ICreatorService
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public CreatorService(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public async Task<ErrorOr<Creator>> GetCreatorById(Guid id)
+    {
+        var creator = await _dbContext.Creators
+            .Include(c => c.Artworks)
+            .ThenInclude(a => a.Images)
+            .SingleOrDefaultAsync(c => c.IdCreator == id);
+
+        if (creator is null)
+        {
+            return CreatorError.NotFound;
+        }
+
+        return creator;
+    }
+
+    public async Task<ErrorOr<Creator>> GetCreatorBySlug(string slug)
+    {
+        var creator = await _dbContext.Creators
+            .Include(c => c.Artworks)
+            .ThenInclude(a => a.Images)
+            .SingleOrDefaultAsync(c => c.Slug == slug);
+
+        if (creator is null)
+        {
+            return CreatorError.NotFound;
+        }
+
+        return creator;
+    }
+}
