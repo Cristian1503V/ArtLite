74fc35f9aa4f089e21f8888d7ad0cec5ca7c40d4
Andresv309 <andresv309@gmail.com>
2023-12-14 05:25:04 -0500

    Add status code errors for actions

---
+++ Server/ArtLite.Api/ServiceErrors/ArtworkError.cs
@@ -0,0 +1,11 @@
+﻿using ErrorOr;
+
+namespace ArtLite.Api.ServiceErrors;
+
+public static class ArtworkError
+{
+    public static Error NotFound => Error.NotFound(
+        code: "Artwork.NotFound",
+        description: "Artwork not found"
+    );
+}
---
+++ Server/ArtLite.Api/ServiceErrors/CreatorError.cs
@@ -0,0 +1,13 @@
+﻿using ErrorOr;
+
+namespace ArtLite.Api.ServiceErrors;
+
+public static class CreatorError
+{
+    public static Error NotFound => Error.NotFound(
+        code: "Creator.NotFound",
+        description: "Creator not found"
+    );
+}
+
+
