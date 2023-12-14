977b7b73bd4ed03955067ab3424e4171527ac9c6
Andresv309 <andresv309@gmail.com>
2023-12-14 05:24:04 -0500

    Add global error controller

---
+++ Server/ArtLite.Api/Controllers/ErrorController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ArtLite.Api.Controllers;
+
+[ApiController]
+[Route("error")]
+public class ErrorController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Error()
+    {
+        return Problem();
+    }
+}
