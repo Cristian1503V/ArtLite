2a7fa264bb9143c99386f933a19365390a882cbe
Andresv309 <andresv309@gmail.com>
2023-12-14 05:25:24 -0500

    Change some namings

--- Server/ArtLite.Api/Services/Artwork/IArtworkService.cs
+++ Server/ArtLite.Api/Services/Artwork/IArtworkService.cs
@@ -6,7 +6,7 @@ namespace ArtLite.Api.Services;
 public interface IArtworkService
 {
     Task<ErrorOr<Artwork>> GetArtworkById(Guid id);
-    Task<ErrorOr<Artwork>> AddArtwork(Artwork artwork);
-    Task<ErrorOr<Artwork>> UpdateArtwork(Guid id, Artwork artwork);
+    Task<ErrorOr<Artwork>> AddArtwork(Artwork newArtwork);
+    Task<ErrorOr<Artwork>> UpdateArtwork(Guid id, Artwork updatedArtwork);
     Task<ErrorOr<Deleted>> DeleteArtwork(Guid id);
 }
