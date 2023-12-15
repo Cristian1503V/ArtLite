d8f563f50f4cce9c978dd8cb0cf76a54bee5c840
Cristian Vega <crisda.arive.1503@gmail.com>
2023-12-15 06:15:35 -0500

    Fix HeaderAuth component

--- client/src/components/header-auth.tsx
+++ client/src/components/header-auth.tsx
@@ -2,35 +2,47 @@ import Link from "next/link"
 import { User } from "@/api/types"
 
 import { ButtonLink } from "./button-link"
-import { AppIcon } from "./icons"
+import { AppIcon, Icons } from "./icons"
 
 interface HeaderAuthProps {
   user?: Pick<User, "profileImage" | "userName">
 }
 
 export function HeaderAuth({ user }: HeaderAuthProps) {
+  const Icon = Icons["upload"]
+
   return (
-    <header className="bg-background sticky top-0 z-50 flex h-16 w-full items-center justify-between px-9 py-8">
-      <Link href={"/"} className="h-8 w-9">
+    <header className="bg-background sticky top-0 z-50 flex h-16 w-full items-center justify-between px-7 py-5">
+      <Link href={"/"}>
         <AppIcon />
       </Link>
-      {user ? (
-        <div>{user.userName}</div>
-      ) : (
-        <div className="flex items-center justify-center gap-4">
-          <ButtonLink
-            href="#"
-            variant="secondary"
-            icon="pencil"
-            className="px-4"
-          >
-            Sign up
-          </ButtonLink>
-          <ButtonLink href="#" icon="logIn" className="px-4">
-            Sign In
-          </ButtonLink>
-        </div>
-      )}
+
+      <div className="flex items-center justify-center gap-4">
+        <Link
+          href={"/artwork/create"}
+          className="bg-muted hover:bg-muted-hover p- rounded-md p-[9px] leading-3 transition-all"
+        >
+          <Icon className="h-4 w-4" />
+        </Link>
+
+        {user ? (
+          <div>{user.userName}</div>
+        ) : (
+          <div className="flex items-center justify-center gap-4">
+            <ButtonLink
+              href="#"
+              variant="secondary"
+              icon="pencil"
+              className="px-4"
+            >
+              Sign up
+            </ButtonLink>
+            <ButtonLink href="#" icon="logIn" className="px-4">
+              Sign In
+            </ButtonLink>
+          </div>
+        )}
+      </div>
     </header>
   )
 }
