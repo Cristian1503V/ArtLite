c6f85efd93397e8ebbbc37251bf5de774c46e3f7
Cristian Vega <crisda.arive.1503@gmail.com>
2023-12-15 06:18:26 -0500

    Add formatBytes  function

--- client/src/libs/utils.ts
+++ client/src/libs/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function formatBytes(bytes: number, decimals = 2) {
+  if (!+bytes) return "0 Bytes"
+  const k = 1024
+  const dm = decimals < 0 ? 0 : decimals
+  const sizes = ["Bytes", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"]
+  const i = Math.floor(Math.log(bytes) / Math.log(k))
+  return `${parseFloat((bytes / Math.pow(k, i)).toFixed(dm))} ${sizes[i]}`
+}
