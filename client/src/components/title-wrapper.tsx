a3a084d5670aba84d44f7a07232e2abb98322571
Cristian Vega <crisda.arive.1503@gmail.com>
2023-12-15 06:16:31 -0500

    Add TitleWrapper component

---
+++ client/src/components/title-wrapper.tsx
@@ -0,0 +1,17 @@
+interface TitleWrapperProps {
+  title: string
+  children: React.ReactNode
+}
+
+export default function TitleWrapper({ title, children }: TitleWrapperProps) {
+  return (
+    <div className="flex flex-col px-4 py-6">
+      <div className="rounded-t-md bg-[#303034] px-3 py-2">
+        <span className="text-sm">{title}</span>
+      </div>
+      <div className="flex flex-col gap-2 rounded-b-md bg-[#18181c] p-4">
+        {children}
+      </div>
+    </div>
+  )
+}
