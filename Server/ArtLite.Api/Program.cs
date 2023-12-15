0a138fc6f20da13c3105f606679cac4ee692de6c
Andresv309 <andresv309@gmail.com>
2023-12-15 01:20:10 -0500

    Add entity configuration for improving logging and performance

--- Server/ArtLite.Api/Program.cs
+++ Server/ArtLite.Api/Program.cs
@@ -4,6 +4,7 @@ using ArtLite.Api.Persistence;
 using ArtLite.Api.Services;
 using ArtLite.Api.Settings;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 
 var builder = WebApplication.CreateBuilder(args);
 {
@@ -13,8 +14,12 @@ var builder = WebApplication.CreateBuilder(args);
     services.AddTransient<DataGenerator>();
     services.AddDbContext<ApplicationDbContext>(options =>
     {
-        options.UseSqlServer("Server=.;Database=DbArtLite;TrustServerCertificate=True;Trusted_Connection=True");
+        options.UseSqlServer(
+            "Server=.;Database=DbArtLite;TrustServerCertificate=True;Trusted_Connection=True",
+            sqlOptions => sqlOptions.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery)
+        );
         options.EnableSensitiveDataLogging();
+        options.ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.MultipleCollectionIncludeWarning));
     });
 
     // services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblyContaining<Program>());
