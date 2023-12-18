ed461b61624edd0684266900838e04dad4e03e51
Cristian Vega <crisda.arive.1503@gmail.com>
2023-12-18 03:13:22 -0500

    Add getArtWork and deleteArtwork enpoins

--- client/src/api/enpoints.ts
+++ client/src/api/enpoints.ts
@@ -2,4 +2,6 @@ import { env } from "@/config/env"
 
 export const enpoints = {
   getCreator: (slung: string) => `${env.api}/creators/${slung}`,
+  getArtwork: (idArtwork: string) => `${env.api}/artworks/${idArtwork}`,
+  deleteArtwork: (idArtwork: string) => `${env.api}/artworks/${idArtwork}`,
 }
