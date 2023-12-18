8907455dcbb9bcc22e46e04e4ae0eacf6742aeed
Cristian Vega <crisda.arive.1503@gmail.com>
2023-12-18 03:12:42 -0500

    Add getArtwork and deleteArtWork

--- client/src/api/actions.ts
+++ client/src/api/actions.ts
@@ -1,9 +1,22 @@
 import axios from "axios"
+import { boolean } from "zod"
 
 import { enpoints } from "./enpoints"
-import { Creator } from "./types"
+import { ArtWork, Creator } from "./types"
 
 export async function getCreator(slug: string) {
   const response = await axios.get<Creator>(enpoints.getCreator(slug))
   return response.data
+}
+
+export async function getArwork(idArtwork: string) {
+  const response = await axios.get<ArtWork>(enpoints.getArtwork(idArtwork))
+  return response.data
+}
+
+export async function deleteArwork(idArtwork: string) {
+  const request = await axios.delete(enpoints.deleteArtwork(idArtwork))
+  const response = request.status
+
+  return response === 204
 }
