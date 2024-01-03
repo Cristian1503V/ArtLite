feb750446b023db0f37d6769d76d838f2216430b
Cristian Vega <crisda.arive.1503@gmail.com>
2024-01-03 18:14:12 -0500

    Add hostname images

--- client/next.config.js
+++ client/next.config.js
@@ -20,6 +20,12 @@ const nextConfig = {
         port: "",
         pathname: "/**",
       },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+        port: "",
+        pathname: "/**",
+      },
     ],
   },
 }
